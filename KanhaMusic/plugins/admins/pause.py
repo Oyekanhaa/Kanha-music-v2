6c4525f5797727a16ebf9e15a0d8a4445e6e4d08
𝐊𝐚𝐧𝐡𝐚 <ayush03kmr@gmail.com>
2025-12-11 15:52:05 +0530

    ˹ sʜʀᴇᴇ ꭙ ϻᴜsɪᴄ ˼

---
+++ KanhaMusic/plugins/admins/pause.py
@@ -0,0 +1,21 @@
+from pyrogram import filters
+from pyrogram.types import Message
+
+from KanhaMusic import kanha
+from KanhaMusic.core.call import Kanha
+from KanhaMusic.utils.database import is_music_playing, music_off
+from KanhaMusic.utils.decorators import AdminRightsCheck
+from KanhaMusic.utils.inline import close_markup
+from config import BANNED_USERS
+
+
+@kanha.on_message(filters.command(["pause", "cpause"]) & filters.group & ~BANNED_USERS)
+@AdminRightsCheck
+async def pause_admin(cli, message: Message, _, chat_id):
+    if not await is_music_playing(chat_id):
+        return await message.reply_text(_["admin_1"])
+    await music_off(chat_id)
+    await Kanha.pause_stream(chat_id)
+    await message.reply_text(
+        _["admin_2"].format(message.from_user.mention), reply_markup=close_markup(_)
+    )
